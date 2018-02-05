1f9a789053d1fdb466e9207a8f25b909d109854b
Eirik Kvalheim <eirikval@uio.no>
2018-02-05 07:09:57 +0100

    Small update

--- Ai.m
+++ Ai.m
@@ -5,6 +5,7 @@ function [ Ai ] = AI(t,a,ai,di)
 % a = Alpha
 % ai = displacement along current X axis
 % di = displacement along previous Z axis
+% cos(), sin() is taking in radiens. For degrees, use cosd(), sind()
 
 Ai = [cos(t), -sin(t)*cos(a),  sin(t)*sin(a), ai*cos(t); 
       sin(t),  cos(t)*cos(a), -cos(t)*sin(a), ai*sin(t); 
