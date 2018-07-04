4e0425e920c7ee9585c2ee63798a062678612035
Eirik Kvalheim <eirikval@uio.no>
2018-07-05 01:12:51 +0200

    Updated to array for inertia tensor instead of 3D matrix

--- Inertia_tensors.m
+++ Inertia_tensors.m
@@ -8,6 +8,10 @@ cL = sym('cL', [6,1]);
 rho = 7850; % kg/m^3 - Steel <-- should be calculated based on weight and volume
 
 
+% Making cell array of inertia tensors
+Inertia_tensors = cell(6,1);
+
+
 % Summing up Inertia tensors for each link, under the assumption that one has uniform solid masses where axis of rotation coincide with center of mass
 for i = 1:n
 
@@ -18,7 +22,7 @@ for i = 1:n
     IzzLi = rho*((aL(i)*bL(i)*cL(i))/12)*(aL(i)^2 + (bL(i))^2);
 
     % 3 Dimensional matrix to hold all link tensor matrices
-    Inertia_tensors(:,:,i) = [IxxLi,     0,     0;
+    Inertia_tensors{i} = [IxxLi,     0,     0;
                                   0, IyyLi,     0;
                                   0,     0, IzzLi]
 
